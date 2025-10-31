010e8f14575944a8105d3d3f0aadf96ce097bfff
themutherfvcker <162934982+themutherfvcker@users.noreply.github.com>
2025-10-31 15:33:04 +1100

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,74 @@
+import Link from 'next/link'
+import { Shield, Phone, Mail, MapPin } from 'lucide-react'
+
+export default function Footer() {
+  return (
+    
+      
+        
+          {/* Company Info */}
+          
+            
+              
+              Northern BeachesRoof Restoration
+            
+            
+              Sydney's trusted roof restoration experts serving the Northern Beaches since 1999.
+            
+          
+
+          {/* Services */}
+          
+            Services
+            
+              Roof Restoration
+              Leak Repairs
+              Roof Painting
+              Roof Cleaning
+            
+          
+
+          {/* Service Areas */}
+          
+            Service Areas
+            
+              Manly
+              Dee Why
+              Mona Vale
+              Brookvale
+              View All Areas
+            
+          
+
+          {/* Contact */}
+          
+            Contact Us
+            
+              
+                
+                1300 000 000
+              
+              
+                
+                info@nbrroof.com.au
+              
+              
+                
+                Serving all Northern Beaches suburbs
+              
+            
+          
+        
+
+        {/* Bottom Bar */}
+        
+          &copy; 2024 Northern Beaches Roof Restoration. All rights reserved.
+          
+            Privacy Policy
+            Terms of Service
+          
+        
+      
+    
+  )
+}
