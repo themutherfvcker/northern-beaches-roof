f10ed85f9a6463e1fd78ae84b858eacfd6e318db
themutherfvcker <162934982+themutherfvcker@users.noreply.github.com>
2025-10-31 15:29:04 +1100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from '@/components/Hero'
+import Services from '@/components/Services'
+import Gallery from '@/components/Gallery'
+import Testimonials from '@/components/Testimonials'
+import Footer from '@/components/Footer'
+
+export default function Home() {
+  return (
+    
+      
+      
+      
+      
+      
+    
+  )
+}
